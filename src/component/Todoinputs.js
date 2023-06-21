dd5dd3be424a5519ff05a317164b3bb7b4d17d9a
VNilawar24 <137284442+VNilawar24@users.noreply.github.com>
2023-06-21 15:53:13 +0530

    Add files via upload

---
+++ src/component/Todoinputs.js
@@ -0,0 +1,24 @@
+// import React,{ useState } from 'react'
+
+// function Todoinputs(props) {
+//   const [inputText , setInputText] = useState('');
+//   return (
+//     <div className = "input-container">
+//         <input 
+//           type="text" 
+//           className='input-box-todo' 
+//           placeholder='Enter your todo'
+//           value = {inputText}
+//           onChange={e=>{
+//             setInputText(e.target.value)
+//           }} />
+//         <button className='add-btn' onClick = {()=>{
+//           props.addList(inputText)
+//           setInputText("")
+//         }}>+</button>
+        
+//     </div>
+//   )
+// }
+
+// export default Todoinputs
